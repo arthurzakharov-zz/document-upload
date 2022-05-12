79088c715f1baa94c2eb498675d3fb6ddd41430a
arthur.zakharov <arthur.zakharov@legal.one>
2022-05-12 16:25:47 +0300

    Add SVGR and logo svg

---
+++ svgo.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    {
+      name: "preset-default",
+    },
+    {
+      name: "prefixIds",
+      active: false,
+    },
+    {
+      name: "removeDimensions",
+      active: true,
+    },
+    {
+      name: "removeTitle",
+      active: true,
+    },
+  ],
+};
