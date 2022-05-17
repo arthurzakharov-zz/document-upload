ec349ed14ff2ee5a056ca42ef82fa055e321751b
arthur.zakharov <arthur.zakharov@legal.one>
2022-05-17 14:57:08 +0300

    Fix Modal for Loadingle

--- src/modals/load-single/load-single.utils.ts
+++ src/modals/load-single/load-single.utils.ts
@@ -4,3 +4,8 @@ export const loadSingleFiles = (open: boolean): string =>
   cn("load-single__files", {
     "load-single__files--open": open,
   });
+
+export const loadSingleSave = (visible: boolean): string =>
+  cn("load-single__save", {
+    "load-single__save--visible": visible,
+  });
