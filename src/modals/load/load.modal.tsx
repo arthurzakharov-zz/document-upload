f982344707260e8b488fddf52b115786164d806a
arthur.zakharov <arthur.zakharov@legal.one>
2022-05-19 15:51:35 +0300

    Add ImageUploader

--- src/modals/load/load.modal.tsx
+++ src/modals/load/load.modal.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable */
 import { useState } from "react";
+import ImageUploading, { ImageListType } from "react-images-uploading";
 import { useDispatch } from "react-redux";
 import { modalClose } from "../../store/modal/modal.actions";
 import SvgUpload from "../../svg/Upload";
@@ -20,6 +22,7 @@ function LoadModal(props: LoadModalProps) {
   const [files, setFiles] = useState<string[]>([]);
   const [fileToClose, setFileToClose] = useState<string>();
   const [title, setTitle] = useState<string>(label);
+  const [images, setImages] = useState<ImageListType>([]);
 
   const dispatch = useDispatch();
 
@@ -38,49 +41,57 @@ function LoadModal(props: LoadModalProps) {
     setTitle(value);
   };
 
+  const onChange = (imageList: ImageListType, addUpdateIndex: number[] | undefined) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList as never[]);
+  };
+
   return (
     <div className="load">
       <div className="load__content">
-        <div className="load__main">
-          <h6 className="load__head">{documentLabel(title, 2)}</h6>
-          <div className="load__info">Wählen Sie hier nur Dateien/Photos Ihres Personalausweises/Reisepasses aus.</div>
-          <div className="load__files">
-            {files.map((file: string, index: number) => (
-              <Collapse key={file} opened={fileToClose !== file} duration={300}>
-                <div
-                  className="load__file"
-                  style={{ marginTop: index === 0 ? 10 : 0, marginBottom: files.length - 1 >= index ? 10 : 0 }}
-                >
-                  <File name={file} onClick={() => onFileClick(file)} />
-                </div>
-              </Collapse>
-            ))}
-          </div>
-          <button
-            type="button"
-            className="load__button"
-            onClick={() => setFiles([...files, Math.ceil(Math.random() * 100).toString()])}
-          >
-            <SvgUpload className="load__button-icon" />
-            <span className="load__button-text">Datei(en) auswählen</span>
-          </button>
-          <p className="load__manual">
-            Maximale Dateigröße pro Datei: 6MB. Unterstützte Dateitypen: PDF, JPG, JPEG, GIF, PNG.
-          </p>
-          {multi ? (
-            <>
-              <hr className="load__line" />
-              <p className="load__rename">Möchten Sie dieses Dokument benennen?</p>
-              <Input
-                id="file-name"
-                name="file-name"
-                placeholder="z.B. Mahnung, Forderungsaufstellung, ..."
-                onChange={onFileNameChange}
-              />
-              <p className="load__tip">Leer lassen wenn unsicher</p>
-            </>
-          ) : null}
-        </div>
+        <ImageUploading multiple maxNumber={5} value={images} onChange={onChange}>
+          {({ onImageUpload }) => (
+            <div className="load__main">
+              <h6 className="load__head">{documentLabel(title, 2)}</h6>
+              <div className="load__info">
+                Wählen Sie hier nur Dateien/Photos Ihres Personalausweises/Reisepasses aus.
+              </div>
+              <div className="load__files">
+                {files.map((file: string, index: number) => (
+                  <Collapse key={file} opened={fileToClose !== file} duration={300}>
+                    <div
+                      className="load__file"
+                      style={{ marginTop: index === 0 ? 10 : 0, marginBottom: files.length - 1 >= index ? 10 : 0 }}
+                    >
+                      <File name={file} onClick={() => onFileClick(file)} />
+                    </div>
+                  </Collapse>
+                ))}
+              </div>
+              <button className="load__button" onClick={onImageUpload}>
+                <SvgUpload className="load__button-icon" />
+                <span className="load__button-text">Datei(en) auswählen</span>
+              </button>
+              <p className="load__manual">
+                Maximale Dateigröße pro Datei: 6MB. Unterstützte Dateitypen: PDF, JPG, JPEG, GIF, PNG.
+              </p>
+              {multi ? (
+                <>
+                  <hr className="load__line" />
+                  <p className="load__rename">Möchten Sie dieses Dokument benennen?</p>
+                  <Input
+                    id="file-name"
+                    name="file-name"
+                    placeholder="z.B. Mahnung, Forderungsaufstellung, ..."
+                    onChange={onFileNameChange}
+                  />
+                  <p className="load__tip">Leer lassen wenn unsicher</p>
+                </>
+              ) : null}
+            </div>
+          )}
+        </ImageUploading>
       </div>
       <div className="load__save">
         <Collapse opened={files.length > 0}>
