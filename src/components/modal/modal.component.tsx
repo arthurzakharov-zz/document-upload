166151f00527a273c69862677c7c87ad9ff90f58
arthur.zakharov <arthur.zakharov@legal.one>
2022-05-19 10:54:59 +0300

    Add react-transition-group

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -1,76 +1,62 @@
-import { useState, useEffect, useRef, KeyboardEvent, MouseEvent } from "react";
+import { useEffect, useRef, KeyboardEvent, MouseEvent } from "react";
 import { useSelector } from "react-redux";
+import { CSSTransition } from "react-transition-group";
 import { selectModalIsOpened, selectModalMain, selectModalMainProps } from "../../store/modal/modal.selectors";
 import useCloseModal from "../../hooks/useCloseModal";
 import useLockedBody from "../../hooks/useLockedBody";
-import { modal, modalBody } from "./modal.utils";
 import "./modal.css";
 
 function Modal() {
-  const [visible, setVisible] = useState<boolean>(false);
-
   const isOpened = useSelector(selectModalIsOpened);
   const Main = useSelector(selectModalMain);
   const mainProps = useSelector(selectModalMainProps);
 
-  const overlayRef = useRef<HTMLDivElement>(null);
+  const modalRef = useRef<HTMLDivElement>(null);
 
   const closeModal = useCloseModal();
 
   const { setLocked } = useLockedBody();
 
   useEffect(() => {
-    if (overlayRef.current && visible) {
-      overlayRef.current.addEventListener("animationend", () => {
-        // eslint-disable-next-line no-console
-        console.log("finish", isOpened);
-      });
-    }
-  }, [visible]);
-
-  useEffect(() => {
     setLocked(isOpened);
     if (isOpened) {
-      setVisible(isOpened);
       setTimeout(() => {
-        if (overlayRef.current) {
+        if (modalRef.current) {
           // @ts-ignore
-          overlayRef.current.focus();
+          modalRef.current.focus();
         }
-      }, 0);
-    } else {
-      setTimeout(() => {
-        setVisible(isOpened);
-      }, 1000);
+      }, 300);
     }
   }, [isOpened, setLocked]);
 
   const overlayClick = (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
-    if (e.target === overlayRef.current) {
+    if (e.target === modalRef.current) {
       closeModal();
     }
   };
 
   const overlayPress = (e: KeyboardEvent<HTMLDivElement>) => {
     e.preventDefault();
-    if (e.target === overlayRef.current && e.key === "Enter") {
+    if (e.target === modalRef.current && e.key === "Enter") {
       closeModal();
     }
   };
 
-  return visible ? (
-    <div
-      ref={overlayRef}
-      tabIndex={0}
-      role="button"
-      className={modal(isOpened)}
-      onKeyUp={overlayPress}
-      onClick={overlayClick}
+  return (
+    <CSSTransition
+      key={1}
+      in={isOpened}
+      timeout={{ enter: 300, exit: 450 }}
+      nodeRef={modalRef}
+      classNames="modal"
+      unmountOnExit
     >
-      <div className={modalBody(isOpened)}>{Main && <Main {...mainProps} />}</div>
-    </div>
-  ) : null;
+      <div ref={modalRef} tabIndex={0} role="button" className="modal" onKeyUp={overlayPress} onClick={overlayClick}>
+        <div className="modal__body">{Main && <Main {...mainProps} />}</div>
+      </div>
+    </CSSTransition>
+  );
 }
 
 export default Modal;
